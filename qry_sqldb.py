978b64e9f0bde40f6fbb453fb0a0c418d45fdf9b
bdallin <brad.dallin@gmail.com>
2025-03-04 22:27:48 -0500

    Add script to query ChEMBL approved small molecule drugs and save results to CSV

---
+++ qry_sqldb.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+qry_sqldb.py
+
+This script connects to a PostgreSQL database, executes a SQL query
+to retrieve bioactivity data in the ChEMBL database, and saves the
+resulting data to a CSV file.
+
+Input:
+- SQL query as a text file
+
+Output:
+- CSV file
+
+Dependencies:
+- sys
+- argparse
+- pandas
+- adbc_driver_postgresql
+- dotenv
+
+Usage:
+- Ensure that the .env file contains the correct database URI
+  under the key 'CHEMBL_LOCAL_URI'
+- Run the script using Python 3.
+
+Author: Brad Dallin
+Date: March 4, 2025
+"""
+
+# Import modules
+import sys
+import argparse
+import pandas as pd
+import adbc_driver_postgresql.dbapi
+from dotenv import dotenv_values
+
+
+# Read query
+def read_query(file_path: str) -> str:
+    """Read and store SQL query from text file"""
+    with open(file_path, "r",
+              encoding="utf-8") as txt_file:
+        txt = txt_file.read()
+    return txt
+
+
+# Connect to PostgreSQL database
+def sql_query(
+        qry: str,
+        env_path: str = ".env"
+        ) -> pd.DataFrame:
+    """Run SQL query and return as Pandas DataFrame"""
+    config = dotenv_values(env_path)
+    # Test connection
+    try:
+        uri = config["CHEMBL_LOCAL_URI"]
+        conn = adbc_driver_postgresql.dbapi.connect(uri)
+        with conn.cursor() as cur:
+            cur.execute("SELECT 1")
+            assert cur.fetchone() == (1,)
+        print("Database connected successfully\n")
+    except adbc_driver_postgresql.dbapi.DatabaseError as err:
+        print(f"Database not connected successfully: {err}\n")
+        sys.exit(1)
+
+    # Run input qry
+    with conn.cursor() as cur:
+        cur.execute(qry)
+        df = pd.DataFrame(
+            cur.fetchall(),
+            columns=[desc[0] for desc in cur.description]
+        )
+    print(f"{df.shape[0]} molecules found!\n")
+    return df
+
+
+# Main function
+def main(args):
+    """Main function"""
+    sql = read_query(args.input)
+    df = sql_query(sql, args.env_path)
+    # Save to file
+    df.to_csv(
+        args.output,
+        index=False,
+        encoding="utf-8"
+    )
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="qry_sqldb.py",
+        description="Retreive data from ChEMBL data and save to a CSV file",
+    )
+    parser.add_argument(
+        "-i", "--input",
+        action="store",
+        type=str,
+        required=True,
+        help="Text file containing SQL query",
+    )
+    parser.add_argument(
+        "-o", "--output",
+        action="store",
+        type=str,
+        required=True,
+        help="CSV file with queried data",
+    )
+    parser.add_argument(
+        "--env_path",
+        action="store",
+        type=str,
+        default=".env",
+        required=False,
+        help="Path to .env file"
+    )
+    main(parser.parse_args())
---
+++ queries/chembl_approved_small_molecule_drugs.sql
@@ -0,0 +1,25 @@
+SELECT DISTINCT m.chembl_id AS molecule_chembl_id,
+s.canonical_smiles AS smiles,
+-- Molecule Dictionary
+m.molregno, m.pref_name, m.max_phase, m.therapeutic_flag, m.dosed_ingredient,
+m.structure_type, m.chebi_par_id, m.molecule_type, m.first_approval, m.oral,
+m.parenteral, m.topical, m.black_box_warning, m.natural_product, m.first_in_class,
+m.chirality, m.prodrug, m.inorganic_flag, m.usan_year, m.availability_type,
+m.usan_stem, m.polymer_flag, m.usan_substem, m.usan_stem_definition,
+m.indication_class, m.withdrawn_flag, m.chemical_probe, m.orphan,
+-- Compound Properties
+p.mw_freebase, p.alogp, p.hba, p.hbd, p.psa, p.rtb, p.ro3_pass,
+p.num_ro5_violations, p.cx_most_apka, p.cx_most_bpka, p.cx_logp,
+p.cx_logd, p.molecular_species, p.full_mwt, p.aromatic_rings,
+p.heavy_atoms, p.qed_weighted, p.mw_monoisotopic, p.full_molformula,
+p.hba_lipinski, p.hbd_lipinski, p.num_lipinski_ro5_violations,
+p.np_likeness_score
+-- Join tables
+FROM compound_structures s
+    RIGHT JOIN compound_properties p ON s.molregno = p.molregno
+    RIGHT JOIN molecule_dictionary m ON p.molregno = m.molregno
+    JOIN compound_records r ON m.molregno = r.molregno
+    AND m.max_phase = 4
+    AND m.molecule_type = 'Small molecule'
+    AND m.inorganic_flag = 0
+ORDER BY m.first_approval ASC
