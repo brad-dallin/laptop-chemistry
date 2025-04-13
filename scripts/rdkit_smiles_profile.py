2c5b93c7bbf82df927d5b52940f7412c78398e7a
brad-dallin <brad.dallin@gmail.com>
2025-04-13 09:53:49 -0400

    Refactor rdkit_smiles_profile.py for improved type hinting and code organization

--- scripts/rdkit_smiles_profile.py
+++ scripts/rdkit_smiles_profile.py
@@ -17,6 +17,7 @@ import os
 import sys
 import argparse
 import pandas as pd
+from typing import List, Dict, Tuple, Optional
 import rdkit
 from rdkit.Chem import Descriptors
 from rdkit.Chem.MolStandardize import rdMolStandardize
@@ -27,7 +28,7 @@ rdkit.RDLogger.DisableLog('rdApp.*')
 ########################################################################
 ## Functions
 ########################################################################
-def parse_args(argv: list) -> argparse.Namespace:
+def parse_args(argv: List[str]) -> argparse.Namespace:
     """Parse command line arguments"""
     parser = argparse.ArgumentParser(
         prog = "rdkit_smiles_profile.py",
@@ -47,20 +48,22 @@ def parse_args(argv: list) -> argparse.Namespace:
         help = "CSV file with processed SMILES and descriptor columns",
     )
     parser.add_argument(
-        "-c", "--smiles_column",
+        "-c",
+        "--smiles_column",
         action = "store",
         type = str,
         default = "smiles",
         help = "Column name containing SMILES",
     )
     parser.add_argument(
-        "-d", "--descriptors",
+        "-d",
+        "--descriptors",
         action = "store",
         type = str,
         default = "all",
         help = "Names of RDKit descriptors to calculate",
     )
-    args = parser.parse_args(argv)
+    args=parser.parse_args(argv)
     return args
 
 
@@ -72,7 +75,7 @@ def load_smiles(smi: str) -> rdkit.Chem.rdchem.Mol:
         try:
             mol = rdkit.Chem.MolFromSmiles(
                 smi,
-                sanitize = False
+                sanitize=False
             )
             assert mol is not None
         except Exception as e:
@@ -84,30 +87,30 @@ def load_smiles(smi: str) -> rdkit.Chem.rdchem.Mol:
 # Sanitize molecule
 def sanitize_molecule(mol: rdkit.Chem.rdchem.Mol) -> rdkit.Chem.rdchem.Mol:
     """Sanitize molecules with RDKit, keep largest fragment, and uncharge"""
-    largest_frag_app = rdMolStandardize.LargestFragmentChooser()
-    uncharge_app = rdMolStandardize.Uncharger()
     try:
         rdkit.Chem.SanitizeMol(mol)
+        largest_frag_app = rdMolStandardize.LargestFragmentChooser()
+        uncharge_app = rdMolStandardize.Uncharger()
         largest_frag_app.chooseInPlace(mol)
         uncharge_app.unchargeInPlace(mol)
+        return True
     except Exception as e:
         print(f"Error processing SMILES: {rdkit.Chem.MolToSmiles(mol)}")
         print(f"Exception: {e}")
         return False
-    return True
 
 
 # Calculate descriptors
 def calculate_descriptors(
         mol: rdkit.Chem.rdchem.Mol,
-        descriptors: list = []
-    ) -> dict:
+        descriptors: List[str] = ["all"]
+    ) -> Dict[str, any]:
     """Calculate RDKit descriptors"""
     rd_descriptors = {
         "SMILES": rdkit.Chem.MolToSmiles(mol),
     }
     for descriptor, fxn in Descriptors._descList:
-        if descriptor in descriptors or descriptors.lower() == "all":
+        if descriptor in descriptors or descriptors[0].lower() == "all":
             try:
                 value = fxn(mol)
             except Exception as e:
@@ -115,14 +118,16 @@ def calculate_descriptors(
             rd_descriptors[f"rdkit_{descriptor}"] = value
     return rd_descriptors
 
+
 # Process molecules and calculate descriptors
 def process_molecules(
         df: pd.DataFrame,
         smiles_column: str = "smiles",
-        descriptors: list = []
+        descriptors: List[str] = ["all"]
     ) -> tuple:
     """Preprocess molecules with RDKit by checking compatibility"""
     mols = []
+    unique_smiles = []
     skipped_indices = []
     for ii, smi in enumerate(df[smiles_column]):
         mol = load_smiles(smi)
@@ -133,59 +138,51 @@ def process_molecules(
         if status is False:
             skipped_indices.append(ii)
             continue
+        frag_smi = rdkit.Chem.MolToSmiles(mol)
+        if frag_smi in unique_smiles:
+            skipped_indices.append(ii)
+            continue
         rddescriptors = calculate_descriptors(
             mol,
             descriptors = descriptors
         )
         rddescriptors["idx"] = ii
         mols.append(rddescriptors)
+        unique_smiles.append(frag_smi)
     print(f"{len(mols)}/{df.shape[0]} molecules processed!")
     print(f"{df.shape[0]-len(mols)}/{df.shape[0]} molecules skipped!")
     return (mols, skipped_indices)
 
 
 # Main function
-def main(argv):
+def main(argv: List[str]):
     """Main function"""
     args = parse_args(argv)
-    if args.descriptors != "all":
-        args.descriptors = [ dd.strip() for dd in args.descriptors.split(",") ]
+    args.descriptors = [dd.strip() for dd in args.descriptors.split(",")]
     df = pd.read_csv(args.input)
 
-    # Process SMILES
-    mols = []
-    skipped_indices = []
-    for ii, smi in enumerate(df[args.smiles_column]):
-        mol = load_smiles(smi)
-        if mol is None:
-            skipped_indices.append(ii)
-            continue
-        status = sanitize_molecule(mol)
-        if status is False:
-            skipped_indices.append(ii)
-            continue
-        rddescriptors = calculate_descriptors(
-            mol,
-            descriptors = args.descriptors
-        )
-        rddescriptors["idx"] = ii
-        mols.append(rddescriptors)
-    print(f"{len(mols)}/{df.shape[0]} molecules processed!")
-    print(f"{df.shape[0]-len(mols)}/{df.shape[0]} molecules skipped!")
+    # Process molecules
+    mols, skipped_indices = process_molecules(
+        df,
+        smiles_column=args.smiles_column,
+        descriptors=args.descriptors
+    )
 
     # Update dataframe with RDKit descriptors
     mol_df = pd.DataFrame(mols)
     mol_df.set_index("idx", inplace=True)
 
-    mol_df.columns = [ 
-        col if col == "SMILES" else col.lower() 
-        for col in mol_df.columns]
+    mol_df.columns = [col if col == "SMILES" else col.lower()
+                      for col in mol_df.columns]
 
     df = pd.concat([df, mol_df], axis=1)
     cols = df.columns.tolist()
     cols.remove('SMILES')
     cols.insert(0, 'SMILES')
     df = df[cols]
+
+    # Drop skipped indices
+    df = df.drop(index=skipped_indices)
 
     # Save to file
     df.to_csv(
