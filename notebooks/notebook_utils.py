79231f99d0873f65a34b882f9ae11cc985f22e63
brad-dallin <brad.dallin@gmail.com>
2025-05-20 22:58:55 -0400

    Rename show_acs1996 function to draw_molecule_acs1996 and add bond highlighting options

--- notebooks/notebook_utils.py
+++ notebooks/notebook_utils.py
@@ -9,7 +9,7 @@ from PIL import Image
 from io import BytesIO
 
 
-def show_acs1996(
+def draw_molecule_acs1996(
     mol: rdkit.Chem.rdchem.Mol,
     legend: str = "",
     width: int = -1,
@@ -17,6 +17,9 @@ def show_acs1996(
     highlight_atoms: list = None,
     highlight_atom_colors: dict = None,
     highlight_color: tuple = (1, 1, 0.7),
+    highlight_bonds: list = None,
+    highlight_bond_colors: dict = None,
+    highlight_bond_color: tuple = (1, 0.5, 0.5)  # Default bond highlight color
 ) -> Image.Image:
     """
     Draw molecule in ACS1996 format and return as PIL Image.
@@ -36,7 +39,13 @@ def show_acs1996(
     highlight_atom_colors : dict, optional
         Dictionary mapping atom indices to RGB colors, default: None
     highlight_color : tuple, optional
-        RGB color for highlighting atoms, default: (1, 1, 0.7)
+        Default RGB color for highlighting atoms, default: (1, 1, 0.7)
+    highlight_bonds : list, optional
+        List of bond indices to highlight, default: None
+    highlight_bond_colors : dict, optional
+        Dictionary mapping bond indices to RGB colors, default: None
+    highlight_bond_color : tuple, optional
+        Default RGB color for highlighting bonds, default: (1, 0.5, 0.5)
 
     Returns
     -------
@@ -46,27 +55,32 @@ def show_acs1996(
     # Compute 2D coordinates
     rdkit.Chem.rdDepictor.Compute2DCoords(mol)
     rdkit.Chem.rdDepictor.StraightenDepiction(mol)
-
+    
     # Create drawer with specified dimensions
     d2d = Draw.MolDraw2DCairo(width, height)
-
+    
     # Set up atom highlighting
     highlight_atom_map = {}
     if highlight_atoms and not highlight_atom_colors:
         highlight_atom_map = {atom_idx: highlight_color for atom_idx in highlight_atoms}
     elif highlight_atom_colors:
         highlight_atom_map = highlight_atom_colors
-
+    
+    # Set up bond highlighting
+    highlight_bond_map = {}
+    if highlight_bonds and not highlight_bond_colors:
+        highlight_bond_map = {bond_idx: highlight_bond_color for bond_idx in highlight_bonds}
+    elif highlight_bond_colors:
+        highlight_bond_map = highlight_bond_colors
+    
     # Draw the molecule with highlighting
-    Draw.DrawMoleculeACS1996(
-        d2d,
-        mol,
-        legend=legend,
-        highlightAtoms=highlight_atoms or [],
-        highlightAtomColors=highlight_atom_map,
-    )
+    Draw.DrawMoleculeACS1996(d2d, mol, legend=legend, 
+                            highlightAtoms=highlight_atoms or [],
+                            highlightAtomColors=highlight_atom_map,
+                            highlightBonds=highlight_bonds or [],
+                            highlightBondColors=highlight_bond_map)
     d2d.FinishDrawing()
-
+    
     # Convert to PIL Image
     bio = BytesIO(d2d.GetDrawingText())
     return Image.open(bio)
